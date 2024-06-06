3785a84b9ebba171139973d66b88a718d1aac5c9
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-06 11:17:02 -0500

    Examples well explained

--- src/Example4-storing-unmanaged-values.tsx
+++ src/Example4-storing-unmanaged-values.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 
 /**
  * Expensive component
+ * In this example, expensiveValueRef stores the result of an expensive calculation, ensuring that the calculation only occurs once.
  * @returns 
  */
 export function StoringUnmanagedValues() {
@@ -9,6 +10,9 @@ export function StoringUnmanagedValues() {
   const [count, setCount] = useState<number>(0);
 
   const calculateExpensiveValue = () => {
+    /**
+     * if ExpensiveValue is zero, let's do the first and unique calculation
+     */
     if (expensiveValueRef.current === 0) {
       // Perform expensive calculation
       expensiveValueRef.current = /* ... */2+1;
@@ -18,8 +22,13 @@ export function StoringUnmanagedValues() {
 
   return (
     <div>
-      <p>Expensive Value: {calculateExpensiveValue()}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p>Expensive Value (remains the same): {calculateExpensiveValue()}</p>
+      <button 
+        onClick={() => {setCount(count + 1); 
+          console.log("State of the component changed...")}
+      }>
+          Increment
+      </button>
     </div>
   );
 }
