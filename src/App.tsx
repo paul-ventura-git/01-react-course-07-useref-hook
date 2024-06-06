aacdaf19d6ed54d27961f4a482a500f62259e94b
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-06 10:52:35 -0500

    Documenting

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import './App.css'
 import { Example1 } from './Example1'
 import { CustomInput } from './CustomInput'
 import { CustomInput2 } from './CustomInput2'
+import { AccessingDOMElements } from './Example2-accessing-dom-elements'
+import { StoringPreviousValues } from './Example3-storing-previous-values'
+import { StoringUnmanagedValues } from './Example4-storing-unmanaged-values'
+import { ThirdPartyLibraries } from './Example5-interacting-with-3rd-party-libraries'
 
 function App() {
 
   return (
     <>
       <Example1></Example1>
+      <h1>1: Accesing DOM elements</h1>
+      <AccessingDOMElements></AccessingDOMElements>
+      <h1>2: Storing previous values</h1>
+      <StoringPreviousValues value={3}></StoringPreviousValues>
+      <h1>3: Storing unmanaged values</h1>
+      <StoringUnmanagedValues></StoringUnmanagedValues>
+      <h1>3: Third party libraries</h1>
+      <ThirdPartyLibraries data={[3]}></ThirdPartyLibraries>
+
       <CustomInput></CustomInput>
       <CustomInput2></CustomInput2>
     </>
